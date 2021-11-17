caa593773c9ae774be25031890caad4254e58232
Mariko <mkaku98@gmail.com>
2021-11-17 00:51:42 -0800

    array

--- assets/js/game.js
+++ assets/js/game.js
@@ -5,9 +5,19 @@ var playerMoney = 10;
 // You can also log multiple values at once like this
 console.log(playerName, playerAttack, playerHealth);
 
-var enemyName = "Roborto";
+var enemyNames = [" Roborto ", " Amy Android ", " Robo Trumble"];
+console.log(enemyNames);
+console.log(enemyNames[0]);
+console.log(enemyNames[1]);
+console.log(enemyNames[2]);
+console.log(enemyNames.length);
 var enemyHealth = 50;
 var enemyAttack = 12;
+// Game States
+// "WIN" - Player robot has defeated all enemy-robots
+//    * Fight all enemy-robots
+//    * Defeat each enemy-robot
+// "LOSE" - Player robot's health is zero or less
 
 var fight = function() {
   
@@ -21,19 +31,19 @@ if (promptFight === "fight" || promptFight === "FIGHT") {
   
   enemyHealth = enemyHealth - playerAttack;
   console.log(
-    playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining."
+    playerName + " attacked " + enemyNames + ". " + enemyNames + " now has " + enemyHealth + " health remaining."
   );
 
   if (enemyHealth <= 0) {
-    window.alert(enemyName + " has died!");
+    window.alert(enemyNames + " has died!");
   } else {
-    window.alert(enemyName + " still has " + enemyHealth + " health left.");
+    window.alert(enemyNames + " still has " + enemyHealth + " health left.");
   }
 
   
   playerHealth = playerHealth - enemyAttack;
   console.log(
-    enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining."
+    enemyNames + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining."
   );
 
  
